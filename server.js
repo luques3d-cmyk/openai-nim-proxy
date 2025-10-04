48166f794f3577a9ba0382f405b9e15bb31ff1ea
luques3d-cmyk <luques3d@gmail.com>
2025-10-04 15:30:39 -0300

    Update server.js

--- server.js
+++ server.js
@@ -96,7 +96,7 @@ app.post('/v1/chat/completions', async (req, res) => {
       model: nimModel,
       messages: messages,
       temperature: temperature || 0.7,
-      max_tokens: max_tokens || 4096,
+      max_tokens: max_tokens || 2048,
       extra_body: ENABLE_THINKING_MODE ? { chat_template_kwargs: { thinking: true } } : undefined,
       stream: false
     };
